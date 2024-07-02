08d040a4bdd04cf2e7cbc7bd444034f38cc9d65e
Diego <diego.trabalhodev@gmail.com>
2024-07-02 15:16:49 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".menu-hamburguer").click(function() {
+        $(".navbar").toggle();
+    });
+
+    $(".comprar").click(function(){
+        window.location.href = "compra.html"
+    })
+});
+
+
